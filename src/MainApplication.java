da3a05ac3b8ce9b01771504e395bf8844c7e5963
Hossein113 <hos_m18@yahoo.com>
2022-05-28 17:07:30 -0700

    Update MainApplication class add method createArra class for added number

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainApplication {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    private static List<Integer> createArrayList() {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(3);
+        list.add(7);
+        list.add(9);
+        list.add(2);
+        list.add(5);
+        list.add(5);
+        list.add(8);
+        list.add(5);
+        list.add(6);
+        list.add(3);
+        list.add(4);
+        list.add(7);
+        list.add(3);
+        list.add(1);
+
+        int number = list.size();
+        if (list.size() % 2 != 0) {
+            list.remove((number - 1));
+        }
+        return list;
+
+    }
+
+
 }
