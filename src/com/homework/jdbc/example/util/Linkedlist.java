c704f8ea41ce03f9a9d8b770bf56a77ca0c35cd5
Hossein113 <hos_m18@yahoo.com>
2022-05-30 15:49:01 -0700

    update method remove in Linkedlist class

--- src/com/homework/jdbc/example/util/Linkedlist.java
+++ src/com/homework/jdbc/example/util/Linkedlist.java
@@ -43,31 +43,26 @@ public class Linkedlist<E> {
         return true;
     }
 
-    public boolean remove(int index) {
+    /*public boolean remove(int index) {
         if (size != 0) {
             if (size / 2 >= index) {
                 int firstCount = 0;
                 Node<E> curentNode = first;
                 while (true) {
-                    curentNode = curentNode.getNext();
                     if (index == firstCount) {
                         Node<E> next = curentNode.getNext();
                         Node<E> prev = curentNode.getPrev();
                         prev.setNext(next);
                         next.setPrev(prev);
                         break;
-
-                    } else {
-                        firstCount++;
                     }
+                    firstCount++;
+                    curentNode = curentNode.getNext();
                 }
-
             } else if (size / 2 > index) {
                 int endCount = size;
                 Node<E> curentNode = last;
-
                 while (true) {
-
                     curentNode = curentNode.getNext();
                     if (index == endCount) {
                         Node<E> next = curentNode.getNext();
@@ -85,15 +80,72 @@ public class Linkedlist<E> {
             return true;
 
         } else {
-
             return false;
         }
+    }*/
+
+    public boolean remove(int index) {
+        if (index > size - 1) {
+            System.out.println("index no of Range");
+            return false;
+        }
+        if (size != 0) {
+            int form = size / 2;
+            if (form >= index) {
+                int firstCount = 0;
+                Node<E> currentNode = first;
+                while (true) {
+                    if (index == 0) {
+
+                        Node<E> next = currentNode.getNext();
+                        next.setPrev(null);
+                        first = next;
+
+                        break;
+                    } else {
+                        if (index == firstCount) {
+                            Node<E> next = currentNode.getNext();
+                            Node<E> prev = currentNode.getPrev();
+                            if (prev != null) {
+                                prev.setNext(next);
+                            }
+                            next.setPrev(prev);
+                            break;
+                        } else {
+                            currentNode = currentNode.getNext();
+                            firstCount++;
+                        }
+                    }
+                }
+            } else {
+                int endCount = size - 1;
+                Node<E> curentNode = last;
+                while (true) {
+                    if (index == endCount) {
+                        Node<E> next = curentNode.getNext();
+                        Node<E> prev = curentNode.getPrev();
+                        if (next != null) {
+                            next.setPrev(prev);
+                        }
+                        prev.setNext(next);
+                        break;
+                    } else {
+                        curentNode = curentNode.getPrev();
+                        endCount--;
+                    }
+                }
+            }
+            size--;
+            return true;
+        }
+        return false;
     }
 
-
     public boolean isEmpity() {
-
-        return size == 0;
+        if (size == 0) {
+            return true;
+        } else
+            return false;
     }
 
     public boolean exist(E e) {
@@ -115,4 +167,26 @@ public class Linkedlist<E> {
 
         return false;
     }
+
+    public void printList() {
+        Node<E> curentNode = first;
+        System.out.print("List: {");
+        for (int i = 0; i < size; i++) {
+            System.out.print(curentNode + " , ");
+
+            curentNode = curentNode.getNext();
+        }
+
+        System.out.println(" }");
+
+
+    }
+
+
+    public String toString() {
+        return
+                "size=" + size;
+
+    }
 }
+
