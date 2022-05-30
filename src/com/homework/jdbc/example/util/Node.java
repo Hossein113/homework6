c2b91e6c0b574ba854f3d6f97dcf9c7189258975
Hossein113 <hos_m18@yahoo.com>
2022-05-30 10:35:32 -0700

    added Node class

---
+++ src/com/homework/jdbc/example/util/Node.java
@@ -0,0 +1,32 @@
+package com.homework.jdbc.example.util;
+
+public class Node<E> {
+
+    E item;
+    Node<E> next;
+    Node<E> prev;
+
+    Node(Node<E> prev, E element, Node<E> next) {
+        this.item = element;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
+    }
+
+
+}
